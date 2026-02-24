b1899e1a9462e4efc2fd80dd2b245faebba31b68
Huyen <huyen.do@aalto.fi>
2026-02-24 23:30:42 +0200

    add plots

--- utils.py
+++ utils.py
@@ -1,17 +1,24 @@
+from qiskit import QuantumCircuit
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_ibm_runtime import SamplerV2 as Sampler
 
 
-def run_circuit(circuit, shots, backend, optimization_level=0):
+def run_circuit(circuit:QuantumCircuit, shots, backend, optimization_level=0):
 
     pm = generate_preset_pass_manager(optimization_level=optimization_level, backend=backend)
     compiled_qc = pm.run(circuit)
-    
-    sampler = Sampler(mode=backend)
-    
-    job = sampler.run([compiled_qc], shots=shots)
-    result = job.result()[0]
-    histogram = result.data.meas.get_counts()
+    print(backend.name)
+    if backend.name.startswith("fake_"):
+        
+        job = backend.run(compiled_qc, shots=shots)
+        result = job.result()
+        histogram = result.get_counts(compiled_qc)    
+    else: 
+        sampler = Sampler(mode=backend)
+        
+        job = sampler.run([compiled_qc], shots=shots)
+        result = job.result()[0]
+        histogram = result.data.meas.get_counts()
     #print(histogram)
     #histogram = result.get_counts(qc)
     histogram = {key: value/shots for key, value in histogram.items()}
